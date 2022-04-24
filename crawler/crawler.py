e0c42456afd30e8d428045c9a17be8a6d9c3020b
ConspiracyHA <hank08311227@gmail.com>
2022-04-24 10:23:04 +0800

    找到最後一個問題的題號

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,10 +1,34 @@
 # from selenium.webdriver.chrome import webdriver
 from selenium.webdriver import Chrome
+from selenium.common.exceptions import NoSuchElementException
+from time import sleep
+import re
+
 
 driver = Chrome()
 
 url = 'https://leetcode.com/problemset/all/'
 driver.get(url)
+
+
+def go_last_page(driver):
+    driver.implicitly_wait(2)
+    page_navigation = driver.find_element_by_css_selector('nav[role=navigation]')
+    last_button = page_navigation.find_element_by_css_selector('button:nth-child(9)')
+    last_button.click()
+    sleep(3)
+
+
+def get_last_problem_index(driver):
+    problem_container = driver.find_element_by_css_selector('div[role=rowgroup]')
+    last_problem = problem_container.find_element_by_css_selector('div[role=row]:last-child')
+    match = re.search(r'(\d+)\.', last_problem.text)
+    return match.group(1) if match else None
+
+
+go_last_page(driver)
+index = get_last_problem_index(driver)
+print(index)
 
 
 
